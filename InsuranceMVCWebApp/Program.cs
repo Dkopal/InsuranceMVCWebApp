77b8d91e99b6a7966e9f55fa37eff5725daeaa17
Dkopal <109029322+Dkopal@users.noreply.github.com>
2022-10-16 20:41:14 +0200

    Add files via upload

---
+++ InsuranceMVCWebApp/Program.cs
@@ -0,0 +1,39 @@
+using InsuranceMVCWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileSystemGlobbing.Internal;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddDbContext<DataContex>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapControllerRoute(
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}/{secondId?}/{persontype?}");
+
+//app.MapControllerRoute(
+//    name: "remove",
+//    pattern: "{controller=FullPerson}/{action=Remove}/{personId?}/{insuranceId?}/{personType?}");
+
+app.Run();
